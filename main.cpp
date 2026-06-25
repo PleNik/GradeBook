ff559b6debf4aeda4473dee056ee848e9eb4f4a5
plenik <pleshkovn69@gmai.com>
2026-06-25 18:10:52 +0500

    Example#2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
-//Пример 1
-//Определить класс GradeBook с методом displayMessage;
+//Пример 2
+//Определить класс GradeBook с методом, принимающим параметр;
 //создать объект класса GradeBook и вызвать его метод displayMessage.
 
 #include <iostream>
+#include <string>
 
 //определение класса GradeBook
 class GradeBook
 {
 public:
     //метод, выводящий приветствие пользователю GradeBook
-    void displayMessage()
+    void displayMessage(std::string courseName)
     {
-        std::cout << "Welcome to the GradeBook!" << std::endl;
+        std::cout << "Welcome to the GradeBook for\n" << courseName << "!" << std::endl;
     }   //конец метода displayMessage
 
 };  //конец класса GradeBook
@@ -19,8 +20,17 @@ public:
 //функция main начинает исполнение программы
 int main()
 {
+    std::string nameOfCourse;   //строка для хранения названия курса
     GradeBook myGradeBook;  //создать объект класса GradeBook с именем myGradeBook
-    myGradeBook.displayMessage();   //вызвать метод displayMessage() объекта myGradeBook
+
+    //запросить ввод названия курса
+    std::cout << "Please enter the course name: " << std::endl;
+    std::getline(std::cin, nameOfCourse);   //прочитать название с пробелами
+    std::cout << std::endl;
+
+    //вызвать метод displayMessage() объекта myGradeBook и передать ей nameOfCourse
+    //в качестве аргумента
+    myGradeBook.displayMessage(nameOfCourse);
 
     return 0;
 }
